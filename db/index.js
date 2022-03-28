04c86c6ff23cafe6b88538faf9e57d2e899bc7c3
jbiehl88 <jcbiehl8@gmail.com>
2022-03-28 10:49:50 -0500

    added routes

--- db/index.js
+++ db/index.js
@@ -319,6 +319,16 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getAllTags() {
+  try {
+    const { rows } = await client.query(`SELECT * FROM tags`);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
@@ -333,4 +343,5 @@ module.exports = {
   createPostTag,
   addTagsToPost,
   getPostsByTagName,
+  getAllTags,
 };
