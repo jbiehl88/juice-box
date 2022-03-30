fd13c6e9608ef7262f7ecafedd290446f0c581c0
Chudi <chudionubogu@yahoo.com>
2022-03-30 10:05:44 -0500

    finished the project

--- api/tags.js
+++ api/tags.js
@@ -1,13 +1,13 @@
 const express = require("express");
+//const client = require("pg/lib/native/client");
 const tagsRouter = express.Router();
+const { getPostsByTagName, getAllTags } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next();
 });
-
-const { getAllTags } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
   const tags = await getAllTags();
@@ -17,4 +17,27 @@ tagsRouter.get("/", async (req, res) => {
   });
 });
 
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+  // const { post } = req.body;
+  const tagPost = {};
+
+  try {
+    const post = await getPostsByTagName(tagName);
+
+    tagPost.post = post;
+
+    res.send(tagPost);
+
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 module.exports = tagsRouter;
+
+//curl http://localhost:3000/api/tags/%23happy/posts
+
+// read the tagname from the params
